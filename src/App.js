43e3bdaa8f02c1740cf2c4fd2647edf6939777da
vicse <vicse.ore@tecsup.edu.pe>
2018-11-22 23:05:59 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      productos:[],
+      recuperado:false
+    }
+  }
+
+  componentWillMount(){
+    fetch('http://localhost:8000/api/productos/')
+    .then((response) =>{
+      return response.json()
+    })
+    .then((prod) =>{
+      this.setState({
+        productos:prod,
+        recuperado:true
+      })
+    })
+  }
+
   render() {
+    if(this.state.recuperado)
+      return this.mostrarTabla()
+    else
+      return(<div>recuperando datos...</div>)
+  }
+
+  mostrarTabla(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Listado de Productos React</h1>
+        <table border="1">
+           <thead>
+             <tr>
+               <th>Código</th>
+               <th>Descripción</th>
+               <th>Precio</th>
+             </tr>
+           </thead>
+           <tbody>
+             {this.state.productos.map(prod =>{
+               return(
+                 <tr key={prod.codigo}>
+                   <td>{prod.codigo}</td>
+                   <td>{prod.descripcion}</td>
+                   <td>{prod.precio}</td>
+                 </tr>
+               );
+             })}
+           </tbody>
+        </table>
       </div>
     );
   }
